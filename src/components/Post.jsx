9d892357daf65820691c15c975575d807df4a5c6
p3rnil <nicolasacostap@gmail.com>
2020-11-03 15:25:43 +0100

    Add images 💄

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,6 +11,25 @@ const Post = ({ data }) => {
             {data.title}
           </a>
         </h3>
+        <div
+          sx={{
+            width: '100%',
+            maxHeight: '140px',
+            borderRadius: '3px',
+            overflow: 'hidden',
+          }}
+        >
+          <img
+            width="100%"
+            sx={{
+              margin: '-25.5% 0px 0px 0px;',
+              width: '100%',
+              height: 'auto',
+              borderRadius: '3px',
+            }}
+            src={data.img}
+          ></img>
+        </div>
         <small sx={{ variant: 'tags.small' }}>{data.small}</small>
       </header>
       <p sx={{ variant: 'tags.p' }}>{data.text}</p>
