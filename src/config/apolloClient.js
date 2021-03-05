adb60f2d64d1622149091fd7e3d85ed7aa7e3372
P3rni1 <nicolasacostap@gmail.com>
2021-03-05 10:02:35 +0100

    Refactored ♻️

--- src/config/apolloClient.js
+++ src/config/apolloClient.js
@@ -1,8 +1,25 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
-import { endpoint } from './settings'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  uri: process.env.GRAPHCMS_PROJECT_API,
+})
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${
+        process.env.IS_PREVIEW
+          ? process.env.GRAPHCMS_DEV_AUTH_TOKEN
+          : process.env.GRAPHCMS_PROD_AUTH_TOKEN
+      }`,
+    },
+  }
+})
 
 const client = new ApolloClient({
-  uri: endpoint,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({ resultCaching: false }),
 })
 
--- src/config/settings.js
+++
@@ -1,4 +0,0 @@
-const endpoint =
-  'https://api-eu-central-1.graphcms.com/v2/ckgz94szfi2r801xk18k8427j/master'
-
-export { endpoint }
